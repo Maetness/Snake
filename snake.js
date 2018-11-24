48619bbd5e41744b732e4303db39ca0bea965026
Matthias Reichenberger <matthias.reichenberger@stud.fh-rosenheim.de>
2018-11-24 13:59:06 +0100

    newupdate

--- snake.js
+++ snake.js
@@ -1,4 +1,12 @@
 function myFunction() {
-    //alert("Hello");
-    console.log("Hello");
+    alert("Hello");
+    //console.log("Hello");
+}
+
+var form2 = document.getElementById('id01');
+// When the user clicks anywhere outside of the form, close it
+document.onclick = function(event) {     
+    if (event.target == form2) {         
+        form2.style.display = 'none'; 
+    }
 }
