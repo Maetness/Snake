38b2bee17c9cb971a0601a9e0b5c8e28200c7446
Matthias Reichenberger <matthias.reichenberger@stud.fh-rosenheim.de>
2019-01-05 17:09:55 +0100

    css+services

--- Angular/my-app/src/app/components/middle-section/middle-section.component.ts
+++ Angular/my-app/src/app/components/middle-section/middle-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { DbService } from '../../db.service'
+import { setFlagsFromString } from 'v8';
 
 @Component({
   selector: 'app-middle-section',
@@ -10,19 +11,41 @@ import { DbService } from '../../db.service'
 export class MiddleSectionComponent implements OnInit {
 
   constructor(private dbservice : DbService) { }
+  sendtype: "login" | "register";
 
   ngOnInit() {
   }
 public onSubmit(form: NgForm){
-  console.log(form.value);
-  this.dbservice.postUserCreation("matthias", "password").subscribe(data =>{
-   // this. = data;
-    console.log("data5", data); 
-    
-  })
+  console.log("form", form.value);
+  console.log("login", this.sendtype);
+
+  if (this.sendtype=="login") {
+    this.dbservice.postUserLogin("matthias", "password").subscribe(data =>{
+      // this. = data;
+       console.log("data6", data); 
+         
+     })
+  } else if(this.sendtype=="register") {
+    this.dbservice.postUserCreation("matthias", "password").subscribe(data =>{
+      // this. = data;
+       console.log("data5", data); 
+
+    })}  
+ 
+  
+}
+
+  public setLogin(){
+    this.sendtype = "login";
+
+  }
+
+  public setRegister(){
+    this.sendtype = "register";
+  }
   
 
-}
+
 
 
 
--- Angular/my-app/src/app/db.service.ts
+++ Angular/my-app/src/app/db.service.ts
@@ -14,7 +14,7 @@ export class DbService {
     return test2;
   }
   public getOverallHighscores () {
-    let test3 = this.http.get("/api/highscore/best")
+    let test3 = this.http.get("/api/highscore/all")
     console.log("test", test3);
     return test3;
   }
@@ -29,4 +29,24 @@ export class DbService {
     let test5 = this.http.post("/api/user/create", username && password);
     return test5;
   }
+
+  public postUserLogin (username: string, password: string){
+    let test6= this.http.post("/api/user/login", username && password);
+    return test6;
+  }
+
+  //Top-User get services // ranking
+  public getTopUsers (username: string){
+    let topusers = this.http.get("/api/user/best")
+    console.log("TopUsers", topusers);
+    return topusers;  
+  }
+
+  public getUserInfo (username: string){
+    let userinfo = this.http.get("/api/user/info" + name)
+    console.log("user-info", userinfo);
+    return userinfo;
+  }
+
+ 
 }
